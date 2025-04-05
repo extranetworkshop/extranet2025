5da10d668a82056c1dbae3190176e108832ea795
Giovanni Grieco <giovanni@grieco.dev>
2025-04-05 23:33:07 +0200

    Simplify website structure

--- themes/event/tests/cta.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test(`Should display the CTA banner`, async ({ page }) => {
-    await page.goto('/');
-    await expect(page.getByText('Sichern Sie sich jetzt Ihr Ticket!')).toBeVisible();
-});
